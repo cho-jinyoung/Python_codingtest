4a993d984d19cb742c17c090bfdaf37767dc0d5c
cho-jinyoung <54131109+cho-jinyoung@users.noreply.github.com>
2020-07-15 01:00:36 +0900

    입출력 별찍기 - 3,4,5

--- Baekjoon/2440, 2441, 2442.py
+++ Baekjoon/2440, 2441, 2442.py
@@ -19,3 +19,15 @@ for j in range(n,0,-1):
     for i in range(j):
         print('*', end='')
     print('\n', end='')
+    
+# 2442번
+# 첫째 줄에는 별 1개, 둘째 줄에는 별 3개, ..., N번째 줄에는 별 2×N-1개를 찍는 문제, 다만 별은 가운데를 기준으로 대칭
+
+n=int(input())
+
+for j in range(n):
+    for i in range(n-j):
+        print(' ', end='')
+    for i in range((j+1)*2-1):
+        print('*', end='')     
+    print('\n', end='')
