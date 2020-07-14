b1d700b81e928fb4607e40d3e10d9c6493e5da01
cho-jinyoung <54131109+cho-jinyoung@users.noreply.github.com>
2020-07-15 01:29:10 +0900

    입출력 별찍기 - 8,12

--- Baekjoon/2445, 2522.py
+++ Baekjoon/2445, 2522.py
@@ -1,4 +1,10 @@
+# 2445번
 # 첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력
+#*    *
+#**  **
+#******
+#**  **
+#*    *
 
 n=int(input())
 
@@ -15,6 +21,30 @@ for j in range(1,n):
     for i in range(n-j):
         print('*', end='')
     for i in range(j*2):
+        print(' ', end='')
+    for i in range(n-j):
+        print('*', end='')    
+    print('\n', end='')
+
+# 2522번
+# 첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력
+#  *
+# **
+#***
+# **
+#  *
+
+n=int(input())
+
+for j in range(1,n+1):
+    for i in range(n-j):
+        print(' ', end='')
+    for i in range(j):
+        print('*', end='')
+    print('\n', end='')
+    
+for j in range(1,n):        
+    for i in range(j):
         print(' ', end='')
     for i in range(n-j):
         print('*', end='')    
