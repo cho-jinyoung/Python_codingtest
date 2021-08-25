f0343c8fb26c82b523991632a6d426b3969653f2
cho-jinyoung <54131109+cho-jinyoung@users.noreply.github.com>
2021-08-25 13:46:45 +0900

    Update 완주하지 못한 선수.py

--- programmers/완주하지 못한 선수.py
+++ programmers/완주하지 못한 선수.py
@@ -3,8 +3,10 @@
 # 동명이인이 있을 수 있음
 # ex) participant=["mislav", "stanko", "mislav", "ana"], completion=["stanko", "ana", "mislav"], return="mislav"
 def solution(participant, completion):
-    # 처음엔 아래 주석코드로 짰는데 시간복잡도를 생각하지 않아 효율성 부분에서 점수를 받지 못했다
-    '''    
+      
+# 처음 풀이 > 효율성 테스트 실패
+# 처음엔 아래 주석코드로 짰는데 시간복잡도를 생각하지 않아 효율성 부분에서 점수를 받지 못했다
+    '''
     for i in participant:
         a=participant.count(i)
         b=completion.count(i)
@@ -12,11 +14,26 @@ def solution(participant, completion):
         if a!=b:
             return i
     '''
-    # 아래 코드의 시간복잡도는 O(n)
-    participant.sort()
-    completion.sort()
     
-    for i in range(len(participant)-1): # 정렬한 리스트의 처음-중간 부분에 완주하지 못한 선수의 이름이 있는 경우
+# 1번 정답
+    '''
+    participant.sort()
+    completion.sort() # 정렬하면 시간이 조금 빨라짐 
+    
+    for i in range(len(participant)-1):
         if participant[i] != completion[i]:
             return participant[i]
-    return participant[-1] # 정렬한 리스트의 맨 마지막에 완주하지 못한 선수의 이름이 
+    return participant[-1]
+    '''
+    
+# 2번 정답 > 해시 이용    
+    p_hash={}
+    part=0
+    
+    for i in participant:
+        p_hash[hash(i)]=i
+        part+=hash(i)
+    for i in completion:
+        part-=hash(i)
+        
+    return p_hash[part]
