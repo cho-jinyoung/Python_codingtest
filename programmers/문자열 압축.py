e73fd165cb9ca37f7763def173cd17e463b86ee8
cho-jinyoung <54131109+cho-jinyoung@users.noreply.github.com>
2021-12-29 23:27:29 +0900

    Update 문자열 압축.py

--- programmers/문자열 압축.py
+++ programmers/문자열 압축.py
@@ -1,23 +1,23 @@
 # 2020 KAKAO BLIND RECRUITMENT
 
 def solution(s):
-    count=1
-    minn=len(s)
-    for i in range(0, len(s)//2):           # 문자열 단위
-        lists=[]
-        word=s[0:i+1]
-        for j in range(i+1, len(s)+1, i+1): # 압축
-            if word==s[j:j+i+1]:
+    count=1             # 압축 단위 문자열 개수 count
+    minn=len(s)         # 현재 길이 > 최솟값 구하기 위해
+    for i in range(1, len(s)//2+1):         # 문자열 단위 길이
+        lists=[]        # 합친 문자열 저장할 리스트
+        word=s[0:i]     # 단위 길이만큼 자른 문자열
+        for j in range(i, len(s)+1, i):     # 압축 루틴
+            if word==s[j:j+i]:              # 압축 문자열
                 count+=1
             else :
-                if count>1:
+                if count>1: # 문자열 압축 가능
                     lists.append(str(count)+word)
-                else:                       # 1 생략
+                else:       # 압축 불가
                     lists.append(word)
-                word=s[j:j+i+1]
+                word=s[j:j+i]
                 count=1
-            if (len(s)-j)<=i:               # 남는 문자열
+            if (len(s)-j)<=(i-1):           # 남는 문자열
                 lists.append(s[j:])
         string=''.join(lists)
-        minn=min(minn, len(string))
+        minn=min(minn, len(string))         # 가장 짧은 문자열 길이
     return minn
