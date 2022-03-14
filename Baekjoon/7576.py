6eac6c9abd920e9294ab7832bfb1bd9ac02058a0
cho-jinyoung <54131109+cho-jinyoung@users.noreply.github.com>
2022-03-14 15:42:00 +0900

    BFS

--- Baekjoon/7576.py
+++ Baekjoon/7576.py
@@ -2,45 +2,40 @@
 # BFS > 1(익은 토마토)를 기준으로 하루가 지날 때마다 옆에 있는 토마토 익히기, 더 이상 익을 수 있는 토마토가 없을 때까지
 # 며칠 걸리는지 세어야 하므로 DFS가 아닌 BFS
 
+from collections import deque
+import sys
+#sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 n,m=map(int, input().split())   # (m,n)
-queue=[]    # 익은 토마토 인덱스 큐
-
-global count
-count=0
+q=[]
 
 tomato=[[0]*n]*m
 for i in range(m):      # 입력과 동시에 익은 토마토 체크
     tomato[i]=list(map(int, input().split()))
     for j in range(n):
         if tomato[i][j]==1:
-            queue.append([i, j])
-            
-def bfs(queue, tomato):
+            q.append([i, j])            
+def bfs():
     dx=[-1, 0, 1, 0]
-    dy=[0, -1, 0, 1] 
-       
-    global count
-    temp=[]
+    dy=[0, -1, 0, 1]      
+    count=0
+    queue=deque(q)
     
     while queue:
-        grow=queue.pop(0)
-        
+        grow=queue.popleft()
+       
         for i in range(4):  # 주변 토마토 익히기
             xm=grow[0]+dx[i]
             yn=grow[1]+dy[i]
-
-            if 0<=xm<m and 0<=yn<n:
-                if tomato[xm][yn]==0:
-                    tomato[xm][yn]=1          # 익힘  
-                    temp.append([xm,yn])      # 익은 토마토 큐에 추가해서 다음날에 이용
-    if not temp:
+            
+            if 0<=xm<m and 0<=yn<n and tomato[xm][yn]==0:
+                    tomato[xm][yn]=tomato[grow[0]][grow[1]]+1         # 익힘  
+                    queue.append([xm,yn])      # 익은 토마토 큐에 추가해서 다음날에 이용
+                    if count < tomato[grow[0]][grow[1]]:
+                        count=tomato[grow[0]][grow[1]]              
+    if not queue:
         for t in tomato:
             if 0 in t:
-                count=-1
-    elif temp:
-        count+=1
-        queue=temp
-        bfs(queue, tomato)
-                
-bfs(queue, tomato)
-print("count=",count)
+                return -1      
+        return count
+print(bfs())
