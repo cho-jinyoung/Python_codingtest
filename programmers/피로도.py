7c3a2140a6ffa60f165264c12bda5db29c876bfb
cho-jinyoung <54131109+cho-jinyoung@users.noreply.github.com>
2022-11-03 15:43:19 +0900

    Update 피로도.py

--- programmers/피로도.py
+++ programmers/피로도.py
@@ -3,7 +3,7 @@
 # dungeons = [["최소 필요 피로도", "소모 피로도"], ... ] , 최소 필요 피로도 >= 소모 피로도
 # k = 현재 피로도, return = 유저가 탐험할 수 있는 최대 던전 수
 
-
+# 순열 라이브러리 이용
 import itertools
 
 def solution(k, dungeons):
@@ -23,3 +23,22 @@ def solution(k, dungeons):
                 
         answer=max(answer, count)
     return answer
+
+
+
+# DFS 이용 (백트랙)
+def solution(k, dungeons):
+    answer = [0]
+    
+    def dfs(k, count, dungeons, answer):
+        answer[0]=max(answer[0], count)
+        
+        for i in range(len(dungeons)):
+            if k >= dungeons[i][0] and not visited[i]:
+                visited[i]=1
+                dfs(k-dungeons[i][1], count+1, dungeons, answer)
+                visited[i]=0
+    
+    visited=[0 for _ in range(len(dungeons))]
+    dfs(k, 0, dungeons, answer)
+    return answer[0]
